a8a1dd528516850f2bbc5baa19cede18607c4682
ast_yzakharevych <y.zakharevych@astoundcommerce.com>
2019-06-27 17:42:26 +0300

    Updates

---
+++ precache-manifest.2cdad884f12f524f0089ecd14d5d8ba9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a720178970d3eb98c0b7865d49f0ef9c",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "3f4960cb7a55f85a2ddd",
+    "url": "/react_dynamic-list-of-posts/static/css/main.16fb019d.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/react_dynamic-list-of-posts/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "3f4960cb7a55f85a2ddd",
+    "url": "/react_dynamic-list-of-posts/static/js/main.7c05ebd8.chunk.js"
+  },
+  {
+    "revision": "fc988510490290486853",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime~main.69709ad2.js"
+  }
+]);
